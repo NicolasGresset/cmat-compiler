14b9e01a580a6ba839a2e550657deb6c9e5a17a5
sarah <dhivert.sarah@gmamil.com>
2024-01-11 02:38:24 +0100

    ajout test fonction matrice

--- tests/fonction_mat/main.c
+++ tests/fonction_mat/main.c
@@ -19,30 +19,35 @@ int main(void) {
     /*__________printmat__________*/
     printf("A\n");
     printmat(A);
+    printf("B\n");
+    printmat(B);
 
     /*__________cst_op_matrix__________*/
     printf("A + -1\n");
-    /* matrix * Z = cst_op_matrix(A, -1, '+'); */
-    /* printmat(Z); */
-    /* printf("A - -1\n"); */
-    /* Z = cst_op_matrix(A, -1, '-'); */
-    /* printmat(Z); */
-    /* printf("A * (-1)\n"); */
-    /* Z = cst_op_matrix(A, -1, '*'); */
-    /* printmat(Z); */
-    /* printf("A / 2\n"); */
-    /* Z = cst_op_matrix(A, 2, '/'); */
-    /* printmat(Z); */
+    matrix *Z = cst_op_matrix(A, -1, '+');
+    printmat(Z);
+    printf("A - -1\n");
+    Z = cst_op_matrix(A, -1, '-');
+    printmat(Z);
+    printf("A * (-1)\n");
+    Z = cst_op_matrix(A, -1, '*');
+    printmat(Z);
+    printf("A / 2\n");
+    Z = cst_op_matrix(A, 2, '/');
+    printmat(Z);
 
     /*__________add_matrix__________*/
     printf("C = A + B\n");
     matrix *C = add_matrix(A, B);
     printmat(C);
 
-    /*__________transpose_matrix__________*/
-    printf("D = ~C\n");
+    printf("C");
     C->mat[1][1] = 2;
     C->mat[0][1] = 1;
+    printmat(C);
+
+    /*__________transpose_matrix__________*/
+    printf("D = ~C\n");
     matrix *D = transpose_matrix(C);
     printmat(D);
 
@@ -70,6 +75,8 @@ int main(void) {
     /*__________printmat__________*/
     printf("a\n");
     printmat(a);
+    printf("b\n");
+    printmat(b);
 
     /*__________cst_op_matrix__________*/
     printf("a + -1\n");
@@ -78,22 +85,24 @@ int main(void) {
     printf("a - -1\n");
     a = cst_op_matrix(a, -1, '-');
     printmat(a);
-    /* printf("a * (-1)\n"); */
-    /* z = cst_op_matrix(a, -1, '*'); */
-    /* printmat(z); */
-    /* printf("a / 2\n"); */
-    /* z = cst_op_matrix(a, 2, '/'); */
-    /* printmat(z); */
+    printf("a * (-1)\n");
+    z = cst_op_matrix(a, -1, '*');
+    printmat(z);
+    printf("a / 2\n");
+    z = cst_op_matrix(a, 2, '/');
+    printmat(z);
 
     /*__________add_matrix__________*/
     printf("c = a + b\n");
     matrix *c = add_matrix(a, b);
     printmat(c);
 
-    /*__________transpose_matrix__________*/
-    printf("d = ~c\n");
     c->mat[0][1] = 2;
     c->mat[0][0] = 1;
+    printmat(c);
+
+    /*__________transpose_matrix__________*/
+    printf("d = ~c\n");
     matrix *d = transpose_matrix(c);
     printmat(d);
 
