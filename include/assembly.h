4046b66db88ffbc4b04be30c86ad163dd4672378
Nicolas Gresset-Bourgeois <nicolas.gresset-bourgeois@etu.unistra.fr>
2024-01-14 15:12:31 +0100

    ajout de printmat en mips ?

--- include/assembly.h
+++ include/assembly.h
@@ -1,5 +1,6 @@
 /* Ce fichier contient un ensemble de méthodes pour traduire la représentation
-du code 3 adresses intermédiaires en code machine interprétable par une architecture MIPS*/
+du code 3 adresses intermédiaires en code machine interprétable par une
+architecture MIPS*/
 
 #pragma once
 #include "lib.h"
@@ -13,15 +14,18 @@ du code 3 adresses intermédiaires en code machine interprétable par une archit
 
 /**
  * @brief Convertit le code 3 adresses en code MIPS
- * 
- * @param code 
+ *
+ * @param code
  */
 void generate_mips_code(struct code *code);
 
 struct assembly_code {
   int next_float_constant;
-  char *float_zero; // variable réservée par le compilateur
+  char *float_zero;      // variable réservée par le compilateur
   char *string_constant; // variable réservée par le compilateur
+  char *string_newline;  // variable réservée par le compilateur
+  char *string_tab;      // variable réservée par le compilateur
+
   FILE *out; // pointeur vers le fichier de sortie où écrire le code MIPS
   char data[MAX_DATA_SIZE]; // le segment data du code MIPS
   int current_data_offset; // l'offset courant dans le segment data pour pouvoir
@@ -41,17 +45,15 @@ struct assembly_code {
 
 extern char *registers[N_REGISTERS];
 
-
 /**
  * @brief $register_number = valeur de symbol
- * 
- * @param symbol 
- * @param code 
- * @param register_number 
+ *
+ * @param symbol
+ * @param code
+ * @param register_number
  */
 void move_int_symbol(struct symbol *symbol, struct assembly_code *code,
-                     int register_number) ;
-
+                     int register_number);
 
 /**
  * @brief $register_number = valeur de symbol
@@ -63,21 +65,19 @@ void move_int_symbol(struct symbol *symbol, struct assembly_code *code,
 void move_float_symbol(struct symbol *symbol, struct assembly_code *code,
                        int register_number);
 
-
 /**
- * @brief Renvoie 1 si le symbole symbole correspond à une constante flottante ou un
- * identificateur de type flottant, 0 sinon
- * 
- * @param symbol 
- * @return int 
+ * @brief Renvoie 1 si le symbole symbole correspond à une constante flottante
+ * ou un identificateur de type flottant, 0 sinon
+ *
+ * @param symbol
+ * @return int
  */
 int is_symbol_float(struct symbol *symbol);
 
-
 /**
- * @brief Renvoie 1 si le symbole symbole correspond à une constante entière ou un
- * identificateur de type entier, 0 sinon
- * @param symbol 
- * @return int 
+ * @brief Renvoie 1 si le symbole symbole correspond à une constante entière ou
+ * un identificateur de type entier, 0 sinon
+ * @param symbol
+ * @return int
  */
 int is_symbol_int(struct symbol *symbol);
